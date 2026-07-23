e81d1f48e28a8fb309cb2a536368ff883417cab1
Gabriel Travaglini Nogueira <gabriel.tnogueira19@gmail.com>
2026-07-23 16:56:18 -0300

    começando a aprender JS

---
+++ JS/teste.js
@@ -0,0 +1,68 @@
+// const variavel_numero = 7
+// const variavel_numero2 = 8
+
+// const variavel_texto = "JavaScript"
+
+// const variavel_bool = true
+
+// console.log(variavel_numero + variavel_numero2)
+// console.log(variavel_texto)
+// console.log(variavel_bool)
+
+// ----------------------------------------------------------------------
+
+// const nome = "Gabriel"
+// let idade = 16
+// let estudandoJS = true
+// const hobbies = [
+//     "karate",
+//     "videogame",
+//     "comer"
+// ]
+// const endereco = {
+//     "rua": "Rua Consolação",
+//     "cidade": "Limeira",
+//     "CEP": "13484-012"
+// }
+
+// ----------------------------------------------------------------------
+
+// function saudacao(nome) {
+//     return `Olá, ${nome}!`
+// }
+
+// console.log(saudacao("Maria")); // "Olá, Maria!"
+
+// ----------------------------------------------------------------------
+
+// const quadrado = (num) => num*num
+
+// const soma = (a, b) => a + b
+
+// const fatorial = (n) => {
+
+//     if (n <= 1) return 1;
+//     return n * fatorial(n - 1);
+// };
+
+// console.log(quadrado(5))
+// console.log(soma(5, 5))
+// console.log(fatorial(5))
+
+// ----------------------------------------------------------------------
+
+function criarContador(){
+    let contador = 0;
+
+    return function() {
+        contador++;
+        return contador;
+    };
+};
+
+const meuContador = criarContador();
+
+console.log(meuContador())
+console.log(meuContador())
+console.log(meuContador())
+
