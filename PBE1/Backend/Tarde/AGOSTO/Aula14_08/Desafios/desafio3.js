eeff578cefaacb8dc560ff0a2a84012bfa9a3696
Gabriel Travaglini Nogueira <Gabriel.tnogueira19@gmail.com>
2026-08-21 14:54:30 -0300

    Aula 21/08

--- PBE1/Backend/Tarde/AGOSTO/Aula14_08/Desafios/desafio3.js
+++ PBE1/Backend/Tarde/AGOSTO/Aula14_08/Desafios/desafio3.js
@@ -3,31 +3,31 @@ const entrada = require('readline-sync');
 function calcularArea(largura, comprimento){
     return largura*comprimento
 }
-// JEITO 1:
-const largura1 = entrada.questionFloat("Informe a largura do terreno 1: ")
-const comprimento1 = entrada.questionFloat("Informe o comprimento do terreno 1:") 
+// // JEITO 1:
+// const largura1 = entrada.questionFloat("Informe a largura do terreno 1: ")
+// const comprimento1 = entrada.questionFloat("Informe o comprimento do terreno 1:") 
 
-const largura2 = entrada.questionFloat("Informe a largura do terreno 2:")
-const comprimento2 = entrada.questionFloat("Informe o comprimento do terreno 2:") 
+// const largura2 = entrada.questionFloat("Informe a largura do terreno 2:")
+// const comprimento2 = entrada.questionFloat("Informe o comprimento do terreno 2:") 
 
-const largura3 = entrada.questionFloat("Informe a largura do terreno 3:") 
-const comprimento3 = entrada.questionFloat("Informe o comprimento do terreno 3:")
+// const largura3 = entrada.questionFloat("Informe a largura do terreno 3:") 
+// const comprimento3 = entrada.questionFloat("Informe o comprimento do terreno 3:")
 
-const area1 = calcularArea(largura1,comprimento1)
-const area2 = calcularArea(largura2,comprimento2)
-const area3 = calcularArea(largura3,comprimento3)
+// const area1 = calcularArea(largura1,comprimento1)
+// const area2 = calcularArea(largura2,comprimento2)
+// const area3 = calcularArea(largura3,comprimento3)
 
-console.log(`O terreno 1 tem uma area de ${area1} metros quadrados!`);
-console.log(`O terreno 2 tem uma area de ${area2} metros quadrados!`);
-console.log(`O terreno 3 tem uma area de ${area3} metros quadrados!`);
+// console.log(`O terreno 1 tem uma area de ${area1} metros quadrados!`);
+// console.log(`O terreno 2 tem uma area de ${area2} metros quadrados!`);
+// console.log(`O terreno 3 tem uma area de ${area3} metros quadrados!`);
 
 // JEITO 2
-for(let i = 0; i < 3; i++){
-    let largura = entrada.questionFloat(`Informe a largura do terreno ${i+1}: `)
-    let comprimento = entrada.questionFloat(`Informe o comprimento do terreno ${i+1}:`)
+for(let i = 1; i < 4; i++){
+    let largura = entrada.questionFloat(`Informe a largura do terreno ${i}: `)
+    let comprimento = entrada.questionFloat(`Informe o comprimento do terreno ${i}:`)
     
     let area = calcularArea(largura,comprimento)
 
-    console.log(`O terreno ${i+1} tem uma area de ${area} metros quadrados!`);
+    console.log(`O terreno ${i} tem uma area de ${area} metros quadrados!`);
 }
 
