c8c7713e96f20dfa365c7d373f6cfe8a33d03474
Gabriel Travaglini Nogueira <Gabriel.tnogueira19@gmail.com>
2026-09-04 10:45:55 -0300

    SOMATIVA

--- SOMATIVAS/Somativa1.sql
+++ SOMATIVAS/Somativa1.sql
@@ -15,7 +15,7 @@ CREATE TABLE if NOT exists Clientes(
 CREATE TABLE if NOT exists Veiculos(
     id_veiculo INT AUTO_INCREMENT PRIMARY KEY,
 
-    placa_veiculo INT NOT NULL UNIQUE,
+    placa_veiculo VARCHAR(10) NOT NULL UNIQUE,
     marca_veiculo VARCHAR(30) NOT NULL,
     proprietario_veiculo VARCHAR(60) NOT NULL,
     tipo_servico ENUM("Manutenção", "Revisão", "Pintura") DEFAULT "Revisão" NOT NULL,
@@ -25,14 +25,22 @@ CREATE TABLE if NOT exists Veiculos(
 CREATE TABLE if NOT exists Marcas(
     id_marca INT AUTO_INCREMENT PRIMARY KEY,
 
-    fornecedor_marca VARCHAR(50) NOT NULL
-
+    nome_marca VARCHAR(60) NOT NULL,
+    fornecedor_marca VARCHAR(50) NOT NULL,
+    local_origem VARCHAR(50),
+    lancamento_marca DATE,
+    status_marca ENUM("Ativo", "Inativo") DEFAULT "Ativo"  
 )
 
 CREATE TABLE if NOT exists Modelos(
-    id_modelos INT AUTO_INCREMENT PRIMARY KEY
+    id_modelos INT AUTO_INCREMENT PRIMARY KEY,
 
-
+    nome_modelo VARCHAR(50),
+    modelo_veiculo ENUM("Carro", "Moto") DEFAULT "Carro" NOT NULL,
+    valor_modelo DECIMAL(8,2),
+    marca_veiculo VARCHAR(50),
+    ano_veiculo VARCHAR(4),
+    tipo_combustivel ENUM("Gasolina", "Etanol", "Elétrico") DEFAULT "Gasolina"
 )
 
 CREATE TABLE if NOT exists Funcionarios(
@@ -41,7 +49,7 @@ CREATE TABLE if NOT exists Funcionarios(
     nome_funcionario VARCHAR(60) NOT NULL,
     cpf_funcionario CHAR(14) NOT NULL UNIQUE,
     data_nascimento DATE NOT NULL,
-    salario DECIMAL(6,2),
+    salario DECIMAL(7,2),
     cargo VARCHAR(50)
 )
 
@@ -50,7 +58,9 @@ CREATE TABLE if NOT exists Servicos(
 
     duracao_servico INT,
     tipo_servico ENUM("Manutenção", "Revisão", "Pintura") DEFAULT "Revisão" NOT NULL,
-    valor_servico DECIMAL(6,2)
+    descricao_servico VARCHAR(255),
+    valor_servico DECIMAL(7,2),
+    data_termino TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 )
 
 CREATE TABLE if NOT exists Pecas(
@@ -69,7 +79,7 @@ CREATE TABLE if NOT exists ordens_servicos(
 
     funcionario_emissor VARCHAR(60) NOT NULL,
     data_emissao_ordem TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    funcionario_desgnado VARCHAR(60) NOT NULL,
+    funcionario_designado VARCHAR(60) NOT NULL,
     prazo_termino DATE,
     classificacao_ordem ENUM("Risco", "Urgente", "Simples") DEFAULT "Simples"
 )
@@ -82,7 +92,7 @@ CREATE TABLE if NOT exists Pagamentos(
     valor_pagar DECIMAL(6,2) NOT NULL,
     forma_pagamento ENUM("Débito", "Crédito", "Pix", "Dinheiro") DEFAULT "Débito",
     parcelas INT,
-    data_pagamento TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    data_pagamento TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 )
 
 CREATE TABLE if NOT exists Fornecedores(
